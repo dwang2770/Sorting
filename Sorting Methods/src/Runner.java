c65ae6a30c05a30bbec2235bd4f1c5152814fefc
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-01-18 09:23:20 -0500

    ss

--- Sorting Methods/src/Runner.java
+++ Sorting Methods/src/Runner.java
@@ -4,7 +4,8 @@ public class Runner {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		
+		double [] somenumsbutwithadot = {2.1,3.4,5.6,9.2,7.7,5.5};
+		int [] somenums = {2,3,5,6,88,10};
 		String [] xd= {"c","g","d","a","b","f","h","j"};
 		BubbleSort.bubbleSort(xd);
 		for (String x : xd)
--- Sorting Methods/src/SelectionSort.java
+++ Sorting Methods/src/SelectionSort.java
@@ -1,4 +1,20 @@
 
-public class SelectionSort {
-
+public class SelectionSort 
+{
+	public static void selectionSort(double [] list1)
+	{
+		int i =0;
+		double holder = list1[i];
+		
+		for (double x : list1)//need position not value of x :/
+		{
+			if(x < holder)
+			{
+				DoubleSwap.doubleSwap(list1, i, );
+			}
+		}
+		
+		
+	
+	}
 }
