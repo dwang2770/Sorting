8a17d8eeb86a1a3d21ec61c258005089ce97a8ab
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-01-18 09:12:43 -0500

    bubblesort done

--- Sorting Methods/src/BubbleSort.java
+++ Sorting Methods/src/BubbleSort.java
@@ -4,13 +4,26 @@ public class BubbleSort
 	public static void bubbleSort( String [] list1)
 	{
 		int i = 0;
-		while (i < list1.length-1)
+		int swap = 0;
+		while (i<list1.length-1)
 		{
 			if ( list1[i].compareTo(list1[i + 1]) > 0)
 			{
-				Swap.stringSwap
+				Swap.stringSwap(list1, i, i+1);
+				swap ++;
+				i++;
+			}
+			else if ( list1[i].compareTo(list1[i + 1]) < 0)
+			{
+				i++;
 			}
 			
 		}
+		
+		if(swap != 0)
+		{
+		bubbleSort(list1);
+		}
+	
 	}
 }
--- Sorting Methods/src/Runner.java
+++ Sorting Methods/src/Runner.java
@@ -1,9 +1,16 @@
 
 public class Runner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		
+		String [] xd= {"c","g","d","a","b","f","h","j"};
+		BubbleSort.bubbleSort(xd);
+		for (String x : xd)
+		{
+			System.out.print(x);
+		}
 	}
 
 }
